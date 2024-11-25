2814ec79c386dafd401b6bc46e2fc3508dc019eb
AllyciaP <140102579+AllyciaP@users.noreply.github.com>
2024-11-25 15:21:30 -0500

    Finish testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (word = '', number = 3) {
 
-};
+    let newWord = '';
+
+    if (number < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < number; i++) {
+
+            newWord = newWord + word;
+        }
+    }
+    return newWord;
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
