c77057386b248391757ad92ead7e8e45c4238e7a
AllyciaP <140102579+AllyciaP@users.noreply.github.com>
2024-11-25 19:26:06 -0500

    Complete testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,20 @@
-const reverseString = function() {
+const reverseString = function(inputStr = '') {
+
+    const arrayStr = [];
+
+    for (let i = 0; i <=inputStr.length - 1; i++) {
+
+        arrayStr[i] = inputStr[i];
+    }
+
+    let revStr = '';
+
+    for (let i = arrayStr.length-1; i >= 0; i--) {
+
+        revStr += arrayStr[i];
+    }
+
+    return revStr;
 
 };
 
