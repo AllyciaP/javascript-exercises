f00131a83f12d1eb45dca67243d58a308013951d
AllyciaP <140102579+AllyciaP@users.noreply.github.com>
2024-11-26 11:50:21 -0500

    Complete tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(checkArr = [v1='', v2='', v3='', v4=''], r1='', r2='', r3='', r4='') {
+    
+     let newArr = [];
+
+    for (let i = 0; i <= checkArr.length-1; i++) {
+
+        if ((checkArr[i] !== r1) & (checkArr[i] !== r2) & (checkArr[i] !== r3) & (checkArr[i] !== r4)){
+            newArr.push(checkArr[i]);
+        }
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
