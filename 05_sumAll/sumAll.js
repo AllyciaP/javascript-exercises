12a1a4b9a221afd6204307ac6875935fdb89d17f
AllyciaP <140102579+AllyciaP@users.noreply.github.com>
2024-12-14 15:43:45 -0500

    Complete exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (first, end) {
 
+    let sum = 0;
+
+    if (!Number.isInteger(first) || !Number.isInteger(end)) {
+        sum = 'ERROR';
+    } else {
+        if (first < 0 || end < 0) {
+            sum = 'ERROR';
+        } else {
+            if (first > end) {
+                for (let i = end; i <= first; i++) {
+                    sum += i;
+                }
+            } else {
+                for (let i = first; i <= end; i++) {
+                    sum += i;
+                }
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
