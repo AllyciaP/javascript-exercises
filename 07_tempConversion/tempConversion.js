d421e60e25508681a600694c7d81f9d5d3290b34
AllyciaP <140102579+AllyciaP@users.noreply.github.com>
2024-12-16 15:31:51 -0500

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  let convertedTemp;
+  let newTemp;
+  convertedTemp = (tempF - 32) * (5 / 9);
+
+  newTemp = parseFloat(convertedTemp.toFixed(1));
+
+  return newTemp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  let convertedTemp;
+  let newTemp;
+
+  convertedTemp = (tempC * (9 / 5) + 32);
+
+  newTemp = parseFloat(convertedTemp.toFixed(1));
+
+  return newTemp;
 };
 
 // Do not edit below this line
